684091d88328b463f02b062d46c1041df454bd59
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-21 18:48:54 +0000

    step 5

--- sample.py/sample.py
+++ sample.py/sample.py
@@ -1,13 +1,18 @@
+import re
+
 def add(numbers):
     if numbers == "":
         return "0"
-        
+
     elif "," not in numbers:
         return str(numbers)
 
     elif "," in numbers:
-        num_list = numbers.split(",")
+        num_list = re.split(",|\n", numbers)
         num_sum = 0
         for i in num_list:
             num_sum += int(i)
         return str(num_sum)
+
+
+    
