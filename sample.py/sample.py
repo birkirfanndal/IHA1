0f006360560902467f6a92ec8ae74b91a1757f22
Birkir Fanndal <birkirfanndal1999@gmail.com>
2020-03-21 17:48:32 +0000

    lagfaeringar

--- sample.py/sample.py
+++ sample.py/sample.py
@@ -1,7 +1,5 @@
 def add(numbers):
     if numbers == "":
         return "0"
-    elif numbers == "1":
-        return "1"
-    elif numbers == "1,2":
-        return "3"
+    else:
+        return str(numbers)
--- sample.py/test_sample.py
+++ sample.py/test_sample.py
@@ -5,7 +5,7 @@ class SampleTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(""), "0")
         self.assertEqual(add("1"), "1")
-        self.assertEqual(add("1,2"), "3")
+        self.assertEqual(add("10"), "10")
 
 
 
